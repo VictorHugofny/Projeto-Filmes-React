a2c3a412fed284da6bc432481e5a63d2b123faf5
Victor Hugo <victorhugofny@gmail.com>
2022-08-17 22:23:19 -0300

    projeto com React

---
+++ src/views/Filme/index.js
@@ -0,0 +1,9 @@
+function Filme(){
+    return(
+        <div>
+            <h1>Filme</h1>
+        </div>
+    )
+}
+
+export default Filme;
---
+++ src/views/Home/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+        <div>
+            <h1>home</h1>
+        </div>
+    )
+}
+
+export default Home;
